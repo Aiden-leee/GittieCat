23b06dc318696825f4aaacc466d8ece3b9bdc2f9
LDH <dhl@LDHui-MacBook-Pro.local>
2020-12-09 17:33:39 +0900

    세팅 및 구조 정리

---
+++ src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+	<section>
+		<GameSection></GameSection>
+		<AnalysisSection></AnalysisSection>
+		<RelationshipSection></RelationshipSection>
+	</section>
+</template>
+<script>
+import GameSection from '@/components/profile/game/GameSection';
+import RelationshipSection from '@/components/profile/relation/RelationshipSection';
+import AnalysisSection from '@/components/profile/analysis/AnalysisSection.vue';
+export default {
+	components: {
+		GameSection,
+		AnalysisSection,
+		RelationshipSection,
+	},
+};
+</script>
+<style lang="scss" scoped></style>
