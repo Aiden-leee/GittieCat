aaa3362492d7ae56abbd688fa4da46503851a8ff
LDH <dhl@LDHui-MacBook-Pro.local>
2020-12-14 01:58:50 +0900

    chart max-width add

---
+++ src/components/loginTest/Login.vue
@@ -0,0 +1,23 @@
+<template>
+	<card>
+		<div>
+			<h1>로그인 성공</h1>
+			<h2>{{ query }}</h2>
+		</div>
+	</card>
+</template>
+
+<script>
+import Card from '../common/Card.vue';
+
+export default {
+	components: { Card },
+	data() {
+		return {
+			query: this.$route.query,
+		};
+	},
+};
+</script>
+
+<style></style>
